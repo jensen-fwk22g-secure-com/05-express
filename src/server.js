a0ebc63fd9bb3616664de7760c7d034ce48ee4f0
David Andersson <lejonmanen@gmail.com>
2023-01-09 15:24:38 +0100

    static middleware and :id URL parameter

--- src/server.js
+++ src/server.js
@@ -15,6 +15,16 @@ const logger = (req, res, next) => {
 	next()
 }
 app.use( logger )
+app.use( express.static(staticPath) )
+
+/*const authExempel = (req, res, next) => {
+	let path = req.url
+	if( path.startsWith('/secret') ) {
+		res.sendStatus(403)  // forbidden
+	} else {
+		next()  // lämna över till nästa
+	}
+}*/
 
 
 // Routes
@@ -24,14 +34,32 @@ app.get('/', (req, res) => {
 	res.sendFile(path)
 })
 
+const bookData = [
+	{ title: 'Pippi Långstrump', authorName: 'Astrid Lindgren', id: 1 },
+	{ title: 'Mio min Mio', authorName: 'Astrid Lindgren', id: 2 },
+	{ title: 'Bröderna Lejonhjärta', authorName: 'Astrid Lindgren', id: 3 }
+]
 app.get('/api/books', (req, res) => {
-	const bookData = [
-		{ title: 'Pippi Långstrump', authorName: 'Astrid Lindgren', id: 1 },
-		{ title: 'Mio min Mio', authorName: 'Astrid Lindgren', id: 2 },
-		{ title: 'Bröderna Lejonhjärta', authorName: 'Astrid Lindgren', id: 3 }
-	]
 	res.send(bookData)
 })
+app.get('/api/books/:id', (req, res) => {
+	console.log('/api/books/:id')
+	const id = req.params.id
+	
+	let maybeBook = bookData.find(book => book.id === Number(id))
+	if( maybeBook ) {
+		res.send(maybeBook)
+	} else {
+		res.sendStatus(404)
+	}
+})
+/*
+Higher order functions:
+filter - ny lista med alla objekt som matchar ett villkor
+find   - första objektet som matchar ett villkor
+map
+forEach
+*/
 
 app.get('/hello', (req, res) => {
 	console.log('GET /')
